f1d21b960c8893a4d8f3484c5a97d673f4a9dd2c
AurelieG13 <108137928+AurelieG13@users.noreply.github.com>
2023-03-18 10:06:47 +0100

    update migrations

--- migrations/Version20230313133938.php
+++ migrations/Version20230313133938.php
@@ -29,7 +29,7 @@ final class Version20230313133938 extends AbstractMigration
         $this->addSql('CREATE TABLE menu (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE formules_menus (menu_id INT NOT NULL, formule_id INT NOT NULL, INDEX IDX_79256B92CCD7E912 (menu_id), INDEX IDX_79256B922A68F4D1 (formule_id), PRIMARY KEY(menu_id, formule_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE opening_hours (id INT AUTO_INCREMENT NOT NULL, day_of_week VARCHAR(255) NOT NULL, opened_lunch_hour TIME NOT NULL, opened_dinner_hour TIME NOT NULL, closed_lunch_hour TIME NOT NULL, closed_dinner_hour TIME NOT NULL, sort_week INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE restaurant (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, guest_max INT NOT NULL, address VARCHAR(255) NOT NULL, phone_number VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_allergy (user_id INT NOT NULL, allergy_id INT NOT NULL, INDEX IDX_93BC5CBFA76ED395 (user_id), INDEX IDX_93BC5CBFDBFD579D (allergy_id), PRIMARY KEY(user_id, allergy_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE dish ADD CONSTRAINT FK_957D8CB8BCF5E72D FOREIGN KEY (categorie_id) REFERENCES category (id)');
@@ -37,7 +37,7 @@ final class Version20230313133938 extends AbstractMigration
         $this->addSql('ALTER TABLE images ADD CONSTRAINT FK_E01FBE6A14041B84 FOREIGN KEY (menus_id) REFERENCES menu (id)');
         $this->addSql('ALTER TABLE formules_menus ADD CONSTRAINT FK_79256B92CCD7E912 FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE formules_menus ADD CONSTRAINT FK_79256B922A68F4D1 FOREIGN KEY (formule_id) REFERENCES formule (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        // $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE user_allergy ADD CONSTRAINT FK_93BC5CBFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_allergy ADD CONSTRAINT FK_93BC5CBFDBFD579D FOREIGN KEY (allergy_id) REFERENCES allergy (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user CHANGE name name VARCHAR(50) NOT NULL, CHANGE firstname firstname VARCHAR(50) NOT NULL, CHANGE phone_number phone_number VARCHAR(10) DEFAULT NULL');
@@ -51,7 +51,7 @@ final class Version20230313133938 extends AbstractMigration
         $this->addSql('ALTER TABLE images DROP FOREIGN KEY FK_E01FBE6A14041B84');
         $this->addSql('ALTER TABLE formules_menus DROP FOREIGN KEY FK_79256B92CCD7E912');
         $this->addSql('ALTER TABLE formules_menus DROP FOREIGN KEY FK_79256B922A68F4D1');
-        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        // $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('ALTER TABLE user_allergy DROP FOREIGN KEY FK_93BC5CBFA76ED395');
         $this->addSql('ALTER TABLE user_allergy DROP FOREIGN KEY FK_93BC5CBFDBFD579D');
         $this->addSql('DROP TABLE allergy');
@@ -63,7 +63,7 @@ final class Version20230313133938 extends AbstractMigration
         $this->addSql('DROP TABLE menu');
         $this->addSql('DROP TABLE formules_menus');
         $this->addSql('DROP TABLE opening_hours');
-        $this->addSql('DROP TABLE reset_password_request');
+        // $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE restaurant');
         $this->addSql('DROP TABLE user_allergy');
         $this->addSql('ALTER TABLE user CHANGE name name VARCHAR(100) NOT NULL, CHANGE firstname firstname VARCHAR(100) NOT NULL, CHANGE phone_number phone_number VARCHAR(30) DEFAULT NULL');
